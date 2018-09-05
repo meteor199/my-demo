e1e2df04896029a9c057c485bbb2b47d62d1230b
meteor199 <meteor199@qq.com>
2018-09-05 20:12:35 +0800

    cef demo

---
+++ cef-win-form/CefWinForm/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using CefSharp.WinForms;
+
+namespace CefWinForm
+{
+    public partial class Form1 : Form
+    {
+        private readonly ChromiumWebBrowser browser;
+
+        public Form1()
+        {
+            InitializeComponent();
+            browser = new ChromiumWebBrowser("www.baidu.com")
+            {
+                Dock = DockStyle.Fill,
+            };
+            panel1.Controls.Add(browser);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
