44fd856df1c7015b49b13f9fe61539f4def8c83b
meteor199 <meteor199@qq.com>
2018-09-10 17:26:51 +0800

    添加 Advanced TreeView for .NET

---
+++ win-form-treeview-adv/Aga.Controls/Tree/NodeControls/NodeProcess.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms.VisualStyles;
+
+namespace Aga.Controls.Tree.NodeControls
+{
+    public class NodeProcess : BindableControl
+    {
+        public override Size MeasureSize(TreeNodeAdv node)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Draw(TreeNodeAdv node, DrawContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ win-form-treeview-adv/README.md
@@ -0,0 +1,3 @@
+一个扩展的 winform 的treeview控件，fork 自[Advanced TreeView for .NET](https://www.codeproject.com/Articles/14741/WebControls/#_comments)
+
+主要添加一个 ProcessBar控件
